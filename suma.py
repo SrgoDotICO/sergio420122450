f583f90834dd9f1b2e89dbe3684ea4cfa8a70145
SrgoDotICO <125301639+SrgoDotICO@users.noreply.github.com>
2023-02-13 12:39:18 -0600

    Eliminar lineas
    Se eliminan las dos ultimas lineas del programa.

--- suma.py
+++ suma.py
@@ -2,5 +2,3 @@ print("Programa suma dos numeros")
 num1 = int(input("Ingrese numero 1"))
 num2 = int(input("Ingrese numero 2"))
 suma = num1 + num2
-print("Suma = " + suma)
-print("Nueva linea de prueba para comit")
